c04b72e877b542f155b0648ab823fc0dc9a784c2
Quoc Tien <38287791+Tienei@users.noreply.github.com>
2018-11-19 15:53:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var refresh = 0
 
 const { exec } = require('child_process');
 
-exec('$ heroku config:set TEST=test', (err, stdout, stderr) => {
+exec('heroku config:set TEST=test', (err, stdout, stderr) => {
     console.log(`stdout: ${stdout}`);
     console.log(`stderr: ${stderr}`);
 })
