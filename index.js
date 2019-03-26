52a5aacd5e51cb09dfcbf03f4442f75f0e373867
Quoc Tien <38287791+Tienei@users.noreply.github.com>
2019-03-26 13:13:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -482,7 +482,8 @@ ReiSevia, Shienei, FinnHeppu, Hugger, rinku, Rosax, -Seoul`)
 - Added new ranking letter for F
 - Added unranked pp calculation for !c
 - Added !akatd
-- Added !scores`)
+- Added !scores
+- New calculation for Aim, Speed, Acc (!osud)`)
             message.channel.send({embed})
         }
 
@@ -1463,9 +1464,9 @@ With **${mods[0].toUpperCase()}**, **${acc}%** accuracy, **${combo}x** combo and
                 var thing = await mapcalc(beatmapid,modandbit.bitpresent,0,0,0,0,0,0)
                 var detail = mapdetail(modandbit.shortenmod,0,Number(best[i][1].bpm),thing.cs,thing.ar,thing.od,thing.hp)
                 star_avg += thing.star.total
-                aim_avg += thing.star.aim
-                speed_avg += thing.star.speed
-                acc_avg += (Math.pow(scoreacc, 3)/Math.pow(100, 3)) * 1.1 * thing.star.total
+                aim_avg += thing.star.aim * (Math.pow(detail.cs, 0.05) / Math.pow(4, 0.05))
+                speed_avg += thing.star.speed * (Math.pow(detail.bpm, 0.2) / Math.pow(180, 0.2))
+                acc_avg += (Math.pow(scoreacc, 3)/Math.pow(100, 3)) * 1.1 * thing.star.total * (Math.pow(detail.od, 0.05) / (Math.pow(6, 0.05)))
                 bpm_avg += detail.bpm
                 cs_avg += detail.cs
                 ar_avg += detail.ar
