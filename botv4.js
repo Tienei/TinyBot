484955b517b1a899c2a25d3a54be2cd4175d00cd
Quoc Tien Dao <tienquoc43@gmail.com>
2020-02-23 13:31:17 +0700

    Update to v4.1

--- botv4.js
+++ botv4.js
@@ -165,7 +165,7 @@ ${rank} *${beatmap.diff}* | **Scores:** ${best[i].score} | **Combo:** ${best[i].
         }
     }
     if (config.config.debug.osutrack == false) {
-        setInterval(real_time_osu_track, 10000) // 90000
+        setInterval(real_time_osu_track, 90000)
     }
 });
 
