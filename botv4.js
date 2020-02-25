28d7a6657e31e59b61d48f4164d8de965ad89470
Tienei <38287791+Tienei@users.noreply.github.com>
2020-02-25 18:10:08 +0700

    Update botv4.js

--- botv4.js
+++ botv4.js
@@ -170,7 +170,7 @@ ${rank} *${beatmap.diff}* | **Scores:** ${best[i].score} | **Combo:** ${best[i].
         }
     }
     if (config.config.debug.osutrack == false) {
-        setInterval(real_time_osu_track, 90000)
+        setInterval(real_time_osu_track, 120000)
     }
 });
 
