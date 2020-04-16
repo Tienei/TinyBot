1512b0af0a7ee0a7fbad55071644940b5f7930ba
Quoc Tien Dao <tienquoc43@gmail.com>
2020-04-16 18:01:58 +0700

    minor minor fix

--- Functions/osu/precalc.js
+++ Functions/osu/precalc.js
@@ -3,7 +3,7 @@ const calc = require('ojsama')
 
 module.exports = async function (beatmapid) {
     let parser = new calc.parser()
-    let map = (await request.get(`https://osu.ppy.sh/osu/${beatmapid}`)).text
+    let map = (await request.get(`https://osu.ppy.sh/osu/${beatmapid}`).timeout({deadline: 30000})).text
     parser.feed(map)
     return parser
 }
