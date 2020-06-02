c1960bff09f36bcb198ac1d31976119b92668b55
Quoc Tien Dao <tienquoc43@gmail.com>
2020-06-02 20:02:39 +0700

    minor fix

--- Functions/osu/precalc.js
+++ Functions/osu/precalc.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const lz_string = require('lz-string')
 const config = require('./../../config')
 const path = './beatmap-cache/'
+let current_process = []
 
 module.exports = async function (beatmapid) {
     let parser = new calc.parser()
@@ -12,13 +13,17 @@ module.exports = async function (beatmapid) {
         let map = lz_string.decompressFromEncodedURIComponent(decode)
         parser.feed(map)
         return parser
+    } else if (!current_process.includes(`${path}${beatmapid}.osu`)) {
+        console.log("Already saving file")  
     } else {
+        current_process.push(`${path}${beatmapid}.osu`)
         let map = (await request.get(`https://osu.ppy.sh/osu/${beatmapid}`)).text
         if (!config.config.debug.command) {
             let encode = lz_string.compressToEncodedURIComponent(map)
             fs.writeFileSync(`${path}${beatmapid}.osu`, encode)
         }
         parser.feed(map)
+        current_process.shift()
         return parser
     }
 }
