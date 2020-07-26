309b85079107b1b91960362a5ae02efff336073a
Quoc Tien Dao <tienquoc43@gmail.com>
2020-07-26 22:35:29 +0700

    clear garbage

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const bot = new Discord.Client({messageCacheMaxSize: 20, messageCacheLifetime: 300, messageSweepInterval: 600});
 bot.login(process.env.BOT_TOKEN)
 const bancho = require('bancho.js')
 const osu_client = new bancho.BanchoClient({ username: process.env.BANCHO_USERNAME, password: process.env.BANCHO_PASSWORD, limiterTimespan: 60000, limiterPrivate: 270, limiterPublic: 54 });
