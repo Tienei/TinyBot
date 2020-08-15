e1073f41abfdae67c1589e4382166608c7126796
Quoc Tien Dao <tienquoc43@gmail.com>
2020-08-15 21:59:25 +0700

    Update DiscordJS

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
-const { Client } = require('discord.js-light');
+const { Client, Intents } = require('discord.js-light');
+const TinyIntents = new Intents()
+TinyIntents.add('GUILDS', 'GUILD_EMOJIS', 'GUILD_INVITES','GUILD_MESSAGES','GUILD_MESSAGE_REACTIONS','DIRECT_MESSAGES','DIRECT_MESSAGE_REACTIONS')
 const bot = new Client({
     cacheGuilds: true,
     cacheChannels: true,
@@ -8,7 +10,8 @@ const bot = new Client({
     cachePresences: false,
     messageCacheMaxSize: 10, 
     messageCacheLifetime: 60, 
-    messageSweepInterval: 120});
+    messageSweepInterval: 120,
+    ws: {intents: TinyIntents}});
 bot.login(process.env.BOT_TOKEN)
 const bancho = require('bancho.js')
 const osu_client = new bancho.BanchoClient({ username: process.env.BANCHO_USERNAME, password: process.env.BANCHO_PASSWORD, limiterTimespan: 60000, limiterPrivate: 270, limiterPublic: 54 });
