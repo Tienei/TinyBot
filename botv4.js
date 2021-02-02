dfd14262ec960bd9bbe09bf2e66138528f6fb87f
Tienei <tienquoc43@gmail.com>
2021-02-02 22:18:20 +0700

    minor change

--- botv4.js
+++ botv4.js
@@ -189,7 +189,7 @@ ${rank} *${beatmap.diff}* | **Scores:** ${best[i].score} | **Combo:** ${best[i].
         }
     }
     if (config.config.debug.osutrack == false) {
-        setInterval(real_time_osu_track, 240000)
+        setInterval(real_time_osu_track, 300000)
     }
 });
 
